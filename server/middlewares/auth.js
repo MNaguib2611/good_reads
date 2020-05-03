4f0f9394bb7cd99954a32e73788446be03be08fe
Mohammed Naguib <m.naguib2611@gmail.com>
2020-05-03 21:28:18 +0200

    login-register-in-server

---
+++ server/middlewares/auth.js
@@ -0,0 +1,25 @@
+function ensureNotAuthentication(req, res, next) {
+    console.log(req.user);
+    if (req.isAuthenticated()) {
+        // req.flash('error_msg', 'You should logout to view that page')
+        res.redirect('/logoutToView');
+    } else {
+        return next();
+    }
+}
+
+// Access Control -- Middleware used before requesting index page
+//put all routes that needs authentication here
+function ensureAuthentication(req, res, next) {
+    console.log(req.user);
+    
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        req.flash('error_msg', 'You are not authorized to view that page')
+        res.redirect('/loginToView');
+    }
+}
+
+module.exports =
+        { ensureNotAuthentication,ensureAuthentication}
