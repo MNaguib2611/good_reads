fcc875ca61d10c7d24c59171ada9acb8f410a96d
kareemMorsy30 <kareemmorsy30@gmail.com>
2020-05-23 18:20:12 +0200

    select option of dashboard sidebar

---
+++ client/src/components/admin/sidebar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faAt, faThLarge, faBook } from "@fortawesome/free-solid-svg-icons";
+
+const Sidebar = ({ children }) => {
+    const [selectedOption, setSelectedOption] = useState(1);
+
+    const changeOption = (option) => {
+        setSelectedOption(option);
+    };
+
+    return (
+        <div className="container">
+            <div className="side nav-container">
+                <div className="card">
+                    <ul>
+                        <li className="nav-list" onClick={() => changeOption(1)} style={{backgroundColor: selectedOption == 1 && '#F56D3A'}}><FontAwesomeIcon icon={faThLarge} size="lg"/><span>Categories</span></li>
+                        <li className="nav-list" onClick={() => changeOption(2)} style={{backgroundColor: selectedOption == 2 && '#F56D3A'}}><FontAwesomeIcon icon={faBook} size="lg"/><span>Books</span></li>
+                        <li className="nav-list" onClick={() => changeOption(3)} style={{backgroundColor: selectedOption == 3 && '#F56D3A'}}><FontAwesomeIcon icon={faAt} size="lg"/><span>Authors</span></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="content">
+                <div className="body">
+                    {children}
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Sidebar;
