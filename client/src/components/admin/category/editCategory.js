5962502afd527c7c56c8a9a9937f7ab4c26874e7
ebtsamali <ebtsamali620@gmail.com>
2020-05-25 09:43:22 +0200

    edit in addCategory & editCategory UI

--- client/src/components/admin/category/editCategory.js
+++ client/src/components/admin/category/editCategory.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CategoryForm from './categoryForm';
-import {editCategoryFun} from '../../../API/category'
+import {editCategoryFun} from '../../../API/category';
+import Layout from '../layout';
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 const EditCategoryPage = (props) => (  
-    <div>
-        <CategoryForm 
-            // category={props.category}
-            category={props.location.state.record}
-            onSubmit = { category => {
-                console.log(category);
-                editCategoryFun(props,category);
-            }} 
-        />
-    </div>
+    <Layout>
+        <div className="card_one">
+            <h5>Edit Category</h5>
+            <Link to="/admin/categories/add" className="addIcon"><FontAwesomeIcon icon={faPlusCircle}/></Link>
+        </div>
+        <div className="card_two">
+            <CategoryForm 
+                // category={props.category}
+                category={props.location.state.record}
+                onSubmit = { category => {
+                    console.log(category);
+                    editCategoryFun(props,category);
+                }} 
+            />
+        </div>
+    </Layout>
 );
 
 const mapStateToProps = (state, props) => {
