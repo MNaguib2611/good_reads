3c79382bf995e9c337fb0e7dc3b40604e95235fc
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-05-26 20:41:14 +0200

    logged user bug fixed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,12 +21,12 @@ const Header = ({ dashboard }) => {
           console.log(err);
         });
       }
-
+      console.log(loggedUser)
     return (<div className="header">
         <div className="nav-container">
             <ul>
                 <li className="nav-list"><Link to="/">Home</Link></li>
-                {!dashboard && !loggedUser.isAdmin &&
+                {!dashboard && loggedUser && !loggedUser.isAdmin &&
                 <>
                 <li className="nav-list"><Link  to="/categories">Categories</Link></li>
                 <li className="nav-list"><a href="#">Books</a></li>
