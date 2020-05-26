553b3446e5e83010d89bd1fcc7e429a178dbbb6a
kareemMorsy30 <kareemmorsy30@gmail.com>
2020-05-26 22:12:23 +0200

    retrieve all authors in add and edit book pages

--- client/src/components/admin/book/editBook.js
+++ client/src/components/admin/book/editBook.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faListUl, faImage } from '@fortawesome/free-solid-svg-icons';
-import { Link, Redirect } from "react-router-dom";
+import { faListUl } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
 import Layout from '../layout';
 import BookForm from './bookForm';
 import { edit } from '../../../API/book';
 import { getAllCategories } from '../../../API/category';
+import { all } from '../../../API/author';
 
 const EditBook = ({ location: { state: { record } = {} } = {}, history, categoryReducer } = {}) => {
     const [ book, setBook ] = useState({
@@ -23,11 +24,18 @@ const EditBook = ({ location: { state: { record } = {} } = {}, history, category
         message: ''
     });
 
+    const [ authors, setAuthors ] = useState([]);
+
+    useEffect(() => {
+        all().then(authors => {
+            setAuthors(authors);
+        });
+    }, []);
+
     const fileInput = React.createRef();
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const updateBookUrl = `http://127.0.0.1:5000/books/${record._id}`;
         if(!book.name || !book.category || !book.author){
             setAlert({
                 errors: true,
@@ -42,7 +50,7 @@ const EditBook = ({ location: { state: { record } = {} } = {}, history, category
             return;
         }
         
-        edit(updateBookUrl, book, fileInput).then(response => {
+        edit(record._id, book, fileInput).then(response => {
             setAlert({
                 success: true,
                 message: 'Book data updated successfully',
@@ -67,12 +75,12 @@ const EditBook = ({ location: { state: { record } = {} } = {}, history, category
             {alert.errors || alert.success ?
                 <h5>{alert.message}</h5>
                 :
-                <h5>Add book</h5>
+                <h5>Edit book</h5>
             }
             <Link to="/admin/books" className="addIcon"><FontAwesomeIcon icon={faListUl}/></Link>
         </div>
         <div className="card_two">
-        <BookForm book={book} setBook={setBook} handleSubmit={handleSubmit} fileInput={fileInput} errors={alert.errors} categories={categoryReducer} operation="Edit"/>
+        <BookForm book={book} setBook={setBook} handleSubmit={handleSubmit} fileInput={fileInput} errors={alert.errors} categories={categoryReducer} authors={authors} operation="Edit"/>
         </div>
     </Layout>;
 };
